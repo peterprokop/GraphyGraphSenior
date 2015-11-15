ee90f1aa66a265a7e1229e160863678be126b1c4
Peter Prokop <prokop.petr@gmail.com>
2015-11-15 22:44:39 +0300

    Initial Commit

---
+++ GraphyGraphExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GraphyGraphExample
+//
+//  Created by Peter Prokop on 15/11/15.
+//  Copyright © 2015 Peter Prokop. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
